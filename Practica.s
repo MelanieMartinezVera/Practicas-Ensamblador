bf4dd79ca92a3923344880b8d995f632d086fa4f
MelanieMartinezVera <134463883+MelanieMartinezVera@users.noreply.github.com>
2023-05-29 22:09:10 -0600

    Add files via upload

--- Practica.s
+++ Practica.s
@@ -1,20 +1,18 @@
-; PIC16F887 Configuration Bit Settings
-
 
 ; PIC16F887 Configuration Bit Settings
 
 ; Assembly source line config statements
 
 ; CONFIG1
-  CONFIG  FOSC = INTRC_CLKOUT   ; Oscillator Selection bits (RC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
-  CONFIG  WDTE = ON             ; Watchdog Timer Enable bit (WDT enabled)
-  CONFIG  PWRTE = ON           ; Power-up Timer Enable bit (PWRT disabled)
-  CONFIG  MCLRE = ON          ; RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+  CONFIG  FOSC = INTRC_CLKOUT   ; Oscillator Selection bits (INTOSC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+  CONFIG  PWRTE = OFF           ; Power-up Timer Enable bit (PWRT disabled)
+  CONFIG  MCLRE = ON            ; RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
   CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
   CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
-  CONFIG  BOREN = OFF           ; Brown Out Reset Selection bits (BOR disabled)
-  CONFIG  IESO = OFF            ; Internal External Switchover bit (Internal/External Switchover mode is disabled)
-  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+  CONFIG  BOREN = ON            ; Brown Out Reset Selection bits (BOR enabled)
+  CONFIG  IESO = ON             ; Internal External Switchover bit (Internal/External Switchover mode is enabled)
+  CONFIG  FCMEN = ON            ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
   CONFIG  LVP = OFF             ; Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
 
 ; CONFIG2
@@ -23,89 +21,57 @@
 
 // config statements should precede project file includes.
 #include <xc.inc>
-
-; When assembly code is placed in a psect, it can be manipulated as a
-; whole by the linker and placed in memory.  
-;
-; In this example, barfunc is the program section (psect) name, 'local' means
-; that the section will not be combined with other sections even if they have
-; the same name.  class=CODE means the barfunc must go in the CODE container.
-; PIC18 should have a delta (addressible unit size) of 1 (default) since they
-; are byte addressible.  PIC10/12/16 have a delta of 2 since they are word
-; addressible.  PIC18 should have a reloc (alignment) flag of 2 for any
-; psect which contains executable code.  PIC10/12/16 can use the default
-; reloc value of 1.  Use one of the psects below for the device you use:
-       
-psect   MainCode,global,class=CODE,delta=2 ; PIC10/12/16
-; psect   barfunc,local,class=CODE,reloc=2 ; PIC18
-; Configuración del microcontrolador
-  
-; Inicialización de variables
-    contador        equ     0x20
-    estadoBoton1    equ     0x21
-    estadoBoton2    equ     0x22
-
-; Configuración de puertos
-    banksel TRISB
-    ;movlw   b'00001000'
-    movwf   TRISB       ; RB3 como salida (LED parpadeante)
+  PSECT MainCode,global,class=CODE,delta=2
+  ;Etiqueta de maincode, es donde se encuentra el codigo principal
+  MAIN: ;etiqueta, marca donde inicia el programa principal
+   ;Configuracion de puertos como salida y entrada
+   BANKSEL TRISB ;SELECCIONO EL BANCO DEL PUERTO B
+   BCF TRISB, 3
+   BANKSEL PORTB ;Envia la configuracion del registro al puerto
+   
+   BANKSEL TRISD
+   BCF TRISD, 0
+   BANKSEL PORTD
+   
+   BANKSEL TRISE
+   BSF TRISE, 0
+   BANKSEL PORTE
+   
+   BANKSEL TRISE
+   BSF TRISE, 1
+   BANKSEL PORTE
+   
+   MainLoop: ;inicio de ciclo iterativo principal
+    ;asegurar que el pin se enuentre en 1 logico antes de apagarlo
+    BTFSS	    PORTE, 0 ;revisar el pin 0 del puerto e
+    BCF		    PORTD, 0 ;si el pin 0 del puerto e es 1, entonces saltar y no entrar a la condicion 
+			     ;si el pin 0 del puerto e es 0, entonces entrar a bcf
+    ;apagar, esperar y encender LED 1
+    BCF		    PORTB, 3 ;COLOCAR A 0 EL PIN 3 DEL PUESTO B (APAGAR LED)
+	CALL	    DELAY ;LLAMAR O EJECUTAR LA SUBRUTINA DELAY
+    BSF		    PORTB, 3 ;COLOCAR A 1 EL PIN 3 DEL PUERTO B (ENCENDER EL LED)
+	CALL        DELAY
+    ;revisar si el boton 1 esta presionado
+    BTFSC PORTE, 0 ;revisar el pin 0 del puerto e
+    BSF PORTD, 0 ;si el pin es 0 logico, no entrar a instruccion bsf
+		 ;si el pin es 1 logico, entrar a instruccion bsf
+    ;revisar si el boton 2 esta presionado		 
+    BTFSC PORTE, 1 ;revisar el pin 1 del puerto e
+    BCF PORTD, 0 ;si el pin es 0 logico, no entrar a instruccion bcf
+		 ;si el pin es 1 logico, entrar a instruccion bcf
+		 
+    GOTO MainLoop ;una vez que completa el retraso
     
-    banksel TRISD
-    ;movlw   b'11111110'
-    movwf   TRISD       ; RD0 como entrada (LED condicional)
-
-; Programa principal
-inicio:
-    banksel PORTB
-    bsf     PORTB, 3    ; Encender LED parpadeante
+    DELAY: 
+	MOVLW 10 ;valor de los milisegundos de espera, mover literal de argumento a registro de trabajo
+	MOVWF 0x10 ;mover el registro del trabajo a la direccion de argumento
+	MOVWF 0x11 ;mover el registro del trabajo a la direccion de argumento, contador 
     
-    banksel PORTD
-    btfss   PORTD, 0    ; Si el botón 2 está presionado...
-        goto        apagaLedCondicional
-    
-    btfss   PORTC, 0    ; Si el botón 1 está presionado...
-        goto        enciendeLedCondicional
-    
-apagaLedCondicional:
-    banksel PORTD
-    bcf     PORTD, 0    ; Apagar LED condicional
-    
-sigueParpadeando:
-    banksel PORTB
-    bcf     PORTB, 3    ; Apagar LED parpadeante
-    
-espera:
-    ;movlw   .d100       ; Esperar un tiempo antes de volver a encender el LED parpadeante
-esperaLoop:
-    decfsz  contador, f ; Decrementar contador y saltar si es cero
-        goto        esperaLoop
-    
-reiniciaContador:
-   ; movlw   .d100       ; Reiniciar contador a 100 (decimal)
-    movwf   contador
-    
-vuelveAInicio:
-    goto inicio
-
-enciendeLedCondicional:
-    banksel PORTD
-    bsf     PORTD, 0    ; Encender LED condicional
-    
-sigueParpadeando2:
-    banksel PORTB
-    bcf     PORTB, 3
-    
-espera2:
-    ;movlw   .d100       ; Esperar un tiempo antes de volver a encender el LED parpadeante
-esperaLoop2:
-    decfsz  contador, f ; Decrementar contador y saltar si es cero
-        goto        esperaLoop2
-    
-reiniciaContador2:
-    ;c       ; Reiniciar contador a 100 (decimal)
-    movwf   contador
-    
-vuelveAInicio2:
-goto inicio
-
-end
+    DELAY_LOOP:
+	DECFSZ 0x10, f
+	GOTO DELAY_LOOP
+	DECFSZ 0x11, f
+	GOTO DELAY_LOOP
+	RETLW 0
+	
+    END MAIN
